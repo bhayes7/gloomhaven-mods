d5a93c92930de6013ce579fc6aa001c96cbe57af
bhayes7 <47124828+bhayes7@users.noreply.github.com>
2022-05-17 11:57:10 -0700

    extended modifier deck to work with blessings and curses

--- curse_sim.R
+++ curse_sim.R
@@ -11,22 +11,39 @@ library(magrittr)
 # that should be implementable without much hassle...gonna have to 
 # think about it
 
-# standard modifier deck
+# what if the deck has entries for all 10 blessings and cursings and 
+# a "contains" variable? that would work but it sounds dumb
+
+# STANDARD MODIFIER DECK
 standardDeckMods <- c(rep_len(0, 6), rep_len(1, 5), rep_len(-1, 5),
                       2, -2, NA, NA)
 standardDeckEffects <- c(rep_len(NA, 18), "2x", "null")
 standardDeck <- data.frame(standardDeckMods, standardDeckEffects)
 
-cards <- standardDeck
+# MODIFIER DECK EXTENDED W/ CURSES AND BLESSINGS
+cnbMods <- c(rep_len(NA, 20))
+cnbEffects <- c(rep_len("bless", 10), rep_len("curse", 10))
+
+extendedDeck <- data.frame(c(standardDeckMods, cnbMods), 
+                           c(standardDeckEffects, cnbEffects)) %>%
+  cbind(rep_len(TRUE, 40))
+
+colnames(extendedDeck) <- c("mod", "effect", "in_deck") 
+
+# SIMULATING CARD DRAWS
+cards <- extendedDeck
 
 numCards <- nrow(cards) # just the num of cards in the deck
 drawn <- rep_len(FALSE, numCards) # tracks which cards have been drawn already
 
 # simulates a card draw, resetting the deck if miss or crit is drawn
+# and discarding blessings and curses
 # returns the index of the drawn card
 drawCard <- function(){
   card <- sample(which(!drawn), 1)
   drawn[card] <<- TRUE
+  # TODO check if drawn card was a blessing or curse
+  
   # check if the deck needs to be shuffled (2x or null is drawn)
   if(cards[card, 2] %in% c("2x", "null")){
     drawn <<-rep_len(FALSE, numCards)
